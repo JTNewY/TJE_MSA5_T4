de00b8936bb080cc66872990e5dea01778e08281
tj-bu-702-15 <tj-bu-702-15@DESKTOP-U6VTJ1S>
2024-02-27 19:40:13 +0900

    0227

---
+++ TJE_MSA5_T4/src/JavaMission/Q1.java
@@ -0,0 +1,26 @@
+package JavaMission;
+
+import java.util.Scanner;
+
+public class Q1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("영어 점수 : ");
+		int eng = sc.nextInt();
+		System.out.print("수학 점수 : ");
+		int math = sc.nextInt();
+		System.out.print("자바 점수 : ");
+		int java= sc.nextInt();
+		
+		
+		int sum = eng + math + java;
+		System.out.println("총점 : "+ sum);
+		double avg = sum /3;
+		System.out.println("평균 :"+ (double)avg);
+	
+	}
+
+}
---
+++ TJE_MSA5_T4/src/JavaMission/Q2.java
@@ -0,0 +1,32 @@
+package JavaMission;
+
+import java.util.Iterator;
+import java.util.Scanner;
+
+public class Q2 {
+
+	public static void main(String[] args) {
+	
+		Scanner sc = new Scanner(System.in);
+		
+		int num = sc.nextInt();
+		
+		if(num>0 && num<=100 ) {
+			
+			
+		}
+		
+		
+		
+			
+		
+		
+		
+		
+		
+		System.out.println();
+		
+		sc.close();
+	}
+
+}
