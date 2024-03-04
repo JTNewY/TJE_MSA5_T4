7355be2ac12e0634564bee68e85df4c97f814a64
JTNewY <niner2@naver.com>
2024-03-04 17:36:26 +0900

    6번 문제 03/04

--- TJE_MSA5_T4/src/question/Q6.java
+++ TJE_MSA5_T4/src/question/Q6.java
@@ -10,13 +10,13 @@ public class Q6 {
 		Scanner sc = new Scanner(System.in);
 		
 		int N =sc.nextInt();
-		int count = 0;
+		int count = 1;
 		int arr[] = new int[6];	
 		
 		
-		do {
-			
-			
+		while(count <= N) {
+			System.out.print("["+count+"게임] : ");
+			// 랜덤 입력
 			for (int i = 0 ; i<arr.length; i++) { //숫자 6개 대입
 				int r = (int)(Math.random() *45 +1);
 				arr[i]=r;
@@ -29,18 +29,31 @@ public class Q6 {
 				}
 			}
 			
+			// 선택 정렬 
+			for(int i =0; i<arr.length; i++) {
+				for(int j =i+1; j<arr.length; j++) {
+					if(arr[i] > arr[j]) {
+						int temp = arr[i];
+						arr[i] = arr[j];
+						arr[j] = temp;
+					}
+				}
+			}
 			
+			// 출력
+			for (int i : arr) { //오름차순, 중복X
+				
+				System.out.print(i+" ");
+				
+			}
+			System.out.println();
 			
-		}while(N == count);
+			count++;
+		}
 		
-		System.out.println(count);
+		
 		
 	
-		for (int i : arr) { //오름차순, 중복X
-			
-			System.out.print(i+" ");
-			
-		}
 		
 	}
 }
